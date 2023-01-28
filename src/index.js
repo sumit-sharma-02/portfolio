0f94ef32481f4ef0e9015dc0f89cff01c0274fc7
sumit-sharma-02 <sumitsharma9968@gmail.com>
2023-01-29 01:28:37 +0530

    Update root component and added assets

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BaseProvider, LightTheme } from "baseui";
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
 import "./index.css";
+import "./assets/font-awesome/css/all.css";
+
 import App from "./App";
+
+const engine = new Styletron();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <App />
+      </BaseProvider>
+    </StyletronProvider>
   </React.StrictMode>
 );
